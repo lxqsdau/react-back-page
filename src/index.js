f3c252e2dea24eb6b07d76a7dc850b32686f3240
刘玄清 <18563799715@163.com>
2019-12-11 13:05:44 +0800

    up

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ConfigComponent extends React.Component {
+
+  render () {
+    return (
+      <div>哈哈哈</div>
+    )
+  }
+}
+
+export default ConfigComponent
+
+/**
+ * 向上传递：
+ * 编辑
+ * 新增
+ */
